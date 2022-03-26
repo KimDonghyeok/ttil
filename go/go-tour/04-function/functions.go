b29a1c4a038f00adb7117858cf6f5f2c0e3b5aed
KimDonghyeok <qoophon@gmail.com>
2022-03-27 04:00:21 +0900

    [update] change file name

--- go/go-tour/04-function/functions.go
+++ go/go-tour/04-function/functions.go
@@ -10,6 +10,10 @@ func add2(x, y int) int {
 	return x + y
 }
 
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
 func main() {
 	fmt.Println(add1(42, 133))
 	fmt.Println(add2(42, 133))
